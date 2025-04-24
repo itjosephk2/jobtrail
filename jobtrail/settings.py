accd8f6c131fb8948786216bac81cf23d196dd14
Joe Keane <josephkeane996@gmail.com>
2025-04-24 15:32:10 +0100

    Update settings.py to use Whitenoise and simplify DB config

--- jobtrail/settings.py
+++ jobtrail/settings.py
@@ -6,30 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# Load environment variables from .env file
-load_dotenv()
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-
-# SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 print("DEBUG is", DEBUG)
 
 ALLOWED_HOSTS = [
-    '127.0.0.1', 
+    '127.0.0.1',
     '8000-itjosephk2-jobtrail-wbl9aypm5t2.ws.codeinstitute-ide.net',
     'jobtrail-fb164ceb9f58.herokuapp.com',
 ]
-
 
 CSRF_TRUSTED_ORIGINS = [
     'http://8000-itjosephk2-jobtrail-wbl9aypm5t2.ws.codeinstitute-ide.net',
@@ -37,25 +25,19 @@ CSRF_TRUSTED_ORIGINS = [
     'https://jobtrail-fb164ceb9f58.herokuapp.com',
 ]
 
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    # Allauth apps
+    # Allauth
     'allauth',
     'allauth.account',
-    # Cloudinary
-    'cloudinary',
-    # For ColorField in model
+    # Optional apps
     'colorfield',
-    # For Classes on form elements
     'widget_tweaks',
     # Custom apps
     'job_application',
@@ -107,10 +89,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'jobtrail.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
@@ -125,9 +103,6 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -145,59 +120,31 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by email
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 SITE_ID = 1
 
-# Optional, but recommended
 LOGIN_REDIRECT_URL = '/job-application/dashboard/'
 LOGOUT_REDIRECT_URL = '/'
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-FILE_UPLOAD_MAX_MEMORY_SIZE = 2621440  # 2.5 MB
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv("CLOUDINARY_CLOUD_NAME", ""),
-    'API_KEY': os.getenv("CLOUDINARY_API_KEY", ""),
-    'API_SECRET': os.getenv("CLOUDINARY_SECRET_KEY", ""),
-}
-
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if not DEBUG:
-    STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 print("DATABASE CONFIG:", DATABASES['default'])
