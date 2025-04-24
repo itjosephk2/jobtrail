3cb08a2298e81bacda5e68c7845638bbe13b98c5
Joe Keane <josephkeane996@gmail.com>
2025-04-24 15:28:30 +0100

    feat: tmp print

--- jobtrail/settings.py
+++ jobtrail/settings.py
@@ -200,3 +200,4 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+print("DATABASE CONFIG:", DATABASES['default'])
